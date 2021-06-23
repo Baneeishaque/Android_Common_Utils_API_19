a63c274a12a921047e4636c8a61e2e1f6cf86cfd
Banee Ishaque K <baneeishaque@gmail.com>
2021-06-23 13:55:31 +0530

    U17 & Send SMS

---
+++ common19/src/main/java/ndk/utils_android19/SendSmsFromFast2SmsNetworkTaskForDk.java
@@ -0,0 +1,21 @@
+package ndk.utils_android19;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import ndk.utils_android1.DkConstants;
+
+public class SendSmsFromFast2SmsNetworkTaskForDk extends SendSmsFromFast2SmsNetworkTaskWithResponseParser {
+
+    public SendSmsFromFast2SmsNetworkTaskForDk(String applicationName, Context currentActivityContext, ProgressBar progressBar, View formView) {
+
+        super(applicationName, currentActivityContext, progressBar, formView);
+    }
+
+    @Override
+    public String configureUserAuthorizationKey() {
+
+        return DkConstants.fast2SmsAuthorizationKey;
+    }
+}
